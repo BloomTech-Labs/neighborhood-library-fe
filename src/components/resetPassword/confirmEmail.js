2e9848fe323c004a6b451a7466196a429f2a640b
Carlos Sanchez <carlo.s16@hotmail.com>
2019-12-24 17:59:38 -0500

    added handlers in ./confirmEmail.js

--- src/components/resetPassword/confirmEmail.js
+++ src/components/resetPassword/confirmEmail.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
-
+import './resetPwd.css'
 
 class ConfirmEmail extends Component {
-    
+    state = {
+      email: ''
+    }
+    handleEmailChange = e => {
+      this.setState({
+        email: e.target.value
+      })
+    }
+    resetFormSubmit = e => {
+      console.log('j')
+    }
     render() {
         return (
           <div className="formHolder">
-            <form className="resetPasswordForm">
-                <input name="email" type="email" placeholder="Enter your email"></input>
-                <button className="resetBtn">Reset password</button>
+            <form className="resetPasswordForm" onSubmit={this.resetFormSubmit}>
+              <h1>Enter your email to reset your password</h1>
+              <input name="email" type="email" placeholder="Enter your email" onChange={this.handleEmailChange} autoFocus></input>
+              <button className="resetBtn">Reset password</button>
             </form>
           </div>
         );
